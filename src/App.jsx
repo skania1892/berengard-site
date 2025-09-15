853153eb6dde077ba2393dd2e1c0f448a0ae68f6
steven1892 <steven.kanias.email@gmail.com>
2025-09-15 14:15:27 -0500

    Fix logo import path

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Logo from './assets/Logo1_centered-update1';
+import Logo from './assets/Logo1_centered-update1.png';
 
 export default function App() {
   return (
